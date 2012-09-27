88ef862952fba6a609d3176e0ab2c5e7da29bcc2
Brian LeRoux <brian@westcoastlogic.com>
2012-09-27 13:38:08 +0100

    ok now wat

--- lxjs2012/js/print.js
+++
@@ -1,4 +0,0 @@
-;(function() {
-var pp = window.location.search.match( /print-pdf/gi ) ? 'pdf' : 'paper'
-document.write( '<link rel="stylesheet" href="css/print/' + pp + '.css" type="text/css" media="print">' )
-});
